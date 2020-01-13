37ff06206ef7dadd9b87ea4e218b749201c88068
shuhuan <shuhuan_bruce@icloud.com>
2020-01-13 14:49:19 +0800

    Initial Commit

---
+++ CameraDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CameraDemo
+//
+//  Created by shuhuan on 2020/1/13.
+//  Copyright © 2020 shuhuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by shuhuan on 2020/1/13.
+//  Copyright © 2020 shuhuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
