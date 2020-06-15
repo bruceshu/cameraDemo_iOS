0789949d987656b3786b7cebddfd64c1c3faf337
shuhuan <shuhuan_bruce@icloud.com>
2020-06-15 21:30:02 +0800

    完善前置摄像头采集视频

---
+++ CameraDemo/Assets.xcassets/CaptureSession.m
@@ -0,0 +1,9 @@
+//
+//  CaptureSession.m
+//  CameraDemo
+//
+//  Created by shuhuan on 2020/1/13.
+//  Copyright © 2020 shuhuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ CameraDemo/CaptureSession.h
@@ -0,0 +1,13 @@
+//
+//  CaptureSession.h
+//  CameraDemo
+//
+//  Created by shuhuan on 2020/1/13.
+//  Copyright © 2020 shuhuan. All rights reserved.
+//
+
+#ifndef CaptureSession_h
+#define CaptureSession_h
+
+
+#endif /* CaptureSession_h */
